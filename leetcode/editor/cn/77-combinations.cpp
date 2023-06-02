768d7e6ff0c7dc327ff311b015c3db88cd4c2452
carbo <git@github.com:sakura745/C-.git>
2023-06-02 09:55:07 +0800

    Jun 2, 2023

---
+++ leetcode/editor/cn/77-combinations.cpp
@@ -0,0 +1,126 @@
+/**
+给定两个整数 n 和 k，返回范围 [1, n] 中所有可能的 k 个数的组合。 
+
+ 你可以按 任何顺序 返回答案。 
+
+ 
+
+ 示例 1： 
+
+ 
+输入：n = 4, k = 2
+输出：
+[
+  [2,4],
+  [3,4],
+  [2,3],
+  [1,2],
+  [1,3],
+  [1,4],
+] 
+
+ 示例 2： 
+
+ 
+输入：n = 1, k = 1
+输出：[[1]] 
+
+ 
+
+ 提示： 
+
+ 
+ 1 <= n <= 20 
+ 1 <= k <= n 
+ 
+
+ Related Topics 回溯 👍 1392 👎 0
+
+*/
+#include<bits/stdc++.h>
+
+using namespace std;
+
+/*struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
+};*/
+/*struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};*/
+//void printLinkedList(ListNode* head);
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+public:
+    vector<int> path;
+    vector<vector<int>> res;
+    void backtracking(int n, int k, int startIndex) {
+        if (path.size() == k) {
+            res.push_back(path);
+            return;
+        }
+        for (int i = startIndex; i <= n - (k - path.size()) + 1/*剪枝*/; ++i) {
+            path.push_back(i);
+            backtracking(n, k, i + 1/*循环内用i*/);
+            path.pop_back();//回溯
+        }
+    }
+    vector<vector<int>> combine(int n, int k) {
+        backtracking(n, k, 1);
+        return res;
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+int main()
+{
+    Solution s;
+//    vector<int> a /*initilization*/;
+//    auto x = s. /*function_name*/;
+//    cout << x << endl;
+    /*    ListNode* test = new ListNode(1);
+    test->next = new ListNode(2);
+    test->next->next = new ListNode(6);
+    test->next->next->next = new ListNode(3);
+    test->next->next->next->next = new ListNode(4);
+    test->next->next->next->next->next = new ListNode(5);
+    test->next->next->next->next->next->next = new ListNode(6);*/
+//    ListNode* head = generateRandomLinkedList(MaxSize, MaxValue);
+//    auto x = s. /*function_name*/;
+/*     TreeNode* root = new TreeNode(5);
+    root->left = new TreeNode(4);
+    root->right = new TreeNode(8);
+    root->left->left = new TreeNode(11);
+    root->left->right = new TreeNode();
+    root->right->left = new TreeNode(13);
+    root->right->right = new TreeNode(4);
+    root->left->left->left = new TreeNode(7);
+    root->left->left->right  = new TreeNode(2);
+    root->left->right->left  = new TreeNode();
+    root->left->right->right  = new TreeNode();
+    root->right->left->left = new TreeNode();
+    root->right->left->right  = new TreeNode();
+    root->right->right->left  = new TreeNode();
+    root->right->right->right  = new TreeNode(1);*/
+    
+    
+    return 0;
+}
+
+/*void printLinkedList(ListNode* head) {
+    if(head == nullptr) return;
+    while(head->next != nullptr){//为了调整输出中 "->" 的位置
+        cout << head->val << " -> ";
+        head = head->next;
+    }
+    cout << head->val << endl;//同上
+}*/
