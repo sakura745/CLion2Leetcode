d8bb8c7e162be8c4dcd22002550bd5c3b7fffc35
carbo <git@github.com:sakura745/C-.git>
2023-06-29 01:10:46 +0800

    Jun 29, 2023

--- leetcode/editor/cn/121-best-time-to-buy-and-sell-stock.cpp
+++ leetcode/editor/cn/121-best-time-to-buy-and-sell-stock.cpp
@@ -1,46 +1,39 @@
 /**
-给你一个整数数组 prices ，其中 prices[i] 表示某支股票第 i 天的价格。 
+给定一个数组 prices ，它的第 i 个元素 prices[i] 表示一支给定股票第 i 天的价格。 
 
- 在每一天，你可以决定是否购买和/或出售股票。你在任何时候 最多 只能持有 一股 股票。你也可以先购买，然后在 同一天 出售。 
+ 你只能选择 某一天 买入这只股票，并选择在 未来的某一个不同的日子 卖出该股票。设计一个算法来计算你所能获取的最大利润。 
 
- 返回 你能获得的 最大 利润 。 
+ 返回你可以从这笔交易中获取的最大利润。如果你不能获取任何利润，返回 0 。 
 
  
 
  示例 1： 
 
  
-输入：prices = [7,1,5,3,6,4]
-输出：7
-解释：在第 2 天（股票价格 = 1）的时候买入，在第 3 天（股票价格 = 5）的时候卖出, 这笔交易所能获得利润 = 5 - 1 = 4 。
-     随后，在第 4 天（股票价格 = 3）的时候买入，在第 5 天（股票价格 = 6）的时候卖出, 这笔交易所能获得利润 = 6 - 3 = 3 。
-     总利润为 4 + 3 = 7 。 
+输入：[7,1,5,3,6,4]
+输出：5
+解释：在第 2 天（股票价格 = 1）的时候买入，在第 5 天（股票价格 = 6）的时候卖出，最大利润 = 6-1 = 5 。
+     注意利润不能是 7-1 = 6, 因为卖出价格需要大于买入价格；同时，你不能在买入前卖出股票。
+ 
 
  示例 2： 
 
  
-输入：prices = [1,2,3,4,5]
-输出：4
-解释：在第 1 天（股票价格 = 1）的时候买入，在第 5 天 （股票价格 = 5）的时候卖出, 这笔交易所能获得利润 = 5 - 1 = 4 。
-     总利润为 4 。 
-
- 示例 3： 
-
- 
 输入：prices = [7,6,4,3,1]
 输出：0
-解释：在这种情况下, 交易无法获得正利润，所以不参与交易可以获得最大利润，最大利润为 0 。 
+解释：在这种情况下, 没有交易完成, 所以最大利润为 0。
+ 
 
  
 
  提示： 
 
  
- 1 <= prices.length <= 3 * 10⁴ 
+ 1 <= prices.length <= 10⁵ 
  0 <= prices[i] <= 10⁴ 
  
 
- Related Topics 贪心 数组 动态规划 👍 2138 👎 0
+ Related Topics 数组 动态规划 👍 3007 👎 0
 
 */
 #include<bits/stdc++.h>
@@ -66,30 +59,20 @@ using namespace std;
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
 public:
-    //贪心算法
+    //二维数组
 //    int maxProfit(vector<int>& prices) {
-//        int res = 0;
-//        for (int i = 1/*第二天才能有利润*/; i < prices.size(); ++i) {
-//            res += prices[i] - prices[i - 1] > 0 ? prices[i] - prices[i - 1] : 0;
-////            res += max(prices[i] - prices[i - 1], 0);//等价于上一行
-//        }
-//        return res;
-//    }
-
-    //动态规划，二维数组
-//    int maxProfit(vector<int>& prices) {
-//        vector<vector<int>> dp(prices.size(), {-prices[0], 0});
+//        vector<vector<int>> dp(prices.size(), {-prices[0], 0});//初始化
 //        for (int i = 1; i < prices.size(); ++i) {
-//            dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] - prices[i]);
+//            dp[i][0] = max(dp[i - 1][0], -prices[i]);
 //            dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] + prices[i]);
 //        }
-//        return dp.back()[1];
+//        return dp.back()[1];//一定是手里没有股票的钱多
 //    }
-    //动态规划，一维数组
+    //一维数组
     int maxProfit(vector<int>& prices) {
         vector<int> dp{-prices[0], 0};
         for (int i = 1; i < prices.size(); ++i) {
-            dp[0] = max(dp[0], dp[1] - prices[i]);
+            dp[0] = max(dp[0], -prices[i]);
             dp[1] = max(dp[1], dp[0] + prices[i]);
         }
         return dp.back();
--- leetcode/editor/cn/123-best-time-to-buy-and-sell-stock-iii.cpp
+++ leetcode/editor/cn/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,46 +1,54 @@
 /**
-给你一个整数数组 prices ，其中 prices[i] 表示某支股票第 i 天的价格。 
+给定一个数组，它的第 i 个元素是一支给定的股票在第 i 天的价格。 
 
- 在每一天，你可以决定是否购买和/或出售股票。你在任何时候 最多 只能持有 一股 股票。你也可以先购买，然后在 同一天 出售。 
+ 设计一个算法来计算你所能获取的最大利润。你最多可以完成 两笔 交易。 
 
- 返回 你能获得的 最大 利润 。 
-
- 
-
- 示例 1： 
+ 注意：你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。 
 
  
-输入：prices = [7,1,5,3,6,4]
-输出：7
-解释：在第 2 天（股票价格 = 1）的时候买入，在第 3 天（股票价格 = 5）的时候卖出, 这笔交易所能获得利润 = 5 - 1 = 4 。
-     随后，在第 4 天（股票价格 = 3）的时候买入，在第 5 天（股票价格 = 6）的时候卖出, 这笔交易所能获得利润 = 6 - 3 = 3 。
-     总利润为 4 + 3 = 7 。 
+
+ 示例 1: 
+
+ 
+输入：prices = [3,3,5,0,0,3,1,4]
+输出：6
+解释：在第 4 天（股票价格 = 0）的时候买入，在第 6 天（股票价格 = 3）的时候卖出，这笔交易所能获得利润 = 3-0 = 3 。
+     随后，在第 7 天（股票价格 = 1）的时候买入，在第 8 天 （股票价格 = 4）的时候卖出，这笔交易所能获得利润 = 4-1 = 3 。 
 
  示例 2： 
 
  
 输入：prices = [1,2,3,4,5]
 输出：4
-解释：在第 1 天（股票价格 = 1）的时候买入，在第 5 天 （股票价格 = 5）的时候卖出, 这笔交易所能获得利润 = 5 - 1 = 4 。
-     总利润为 4 。 
+解释：在第 1 天（股票价格 = 1）的时候买入，在第 5 天 （股票价格 = 5）的时候卖出, 这笔交易所能获得利润 = 5-1 = 4 。   
+     注意你不能在第 1 天和第 2 天接连购买股票，之后再将它们卖出。   
+     因为这样属于同时参与了多笔交易，你必须在再次购买前出售掉之前的股票。
+ 
 
  示例 3： 
 
  
-输入：prices = [7,6,4,3,1]
+输入：prices = [7,6,4,3,1] 
+输出：0 
+解释：在这个情况下, 没有交易完成, 所以最大利润为 0。 
+
+ 示例 4： 
+
+ 
+输入：prices = [1]
 输出：0
-解释：在这种情况下, 交易无法获得正利润，所以不参与交易可以获得最大利润，最大利润为 0 。 
+ 
 
  
 
  提示： 
 
  
- 1 <= prices.length <= 3 * 10⁴ 
- 0 <= prices[i] <= 10⁴ 
+ 1 <= prices.length <= 10⁵ 
+ 0 <= prices[i] <= 10⁵ 
  
 
- Related Topics 贪心 数组 动态规划 👍 2138 👎 0
+ Related Topics 数组 动态规划 👍 1438 👎 0
 
 */
 #include<bits/stdc++.h>
@@ -66,31 +74,27 @@ using namespace std;
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
 public:
-    //贪心算法
-//    int maxProfit(vector<int>& prices) {
-//        int res = 0;
-//        for (int i = 1/*第二天才能有利润*/; i < prices.size(); ++i) {
-//            res += prices[i] - prices[i - 1] > 0 ? prices[i] - prices[i - 1] : 0;
-////            res += max(prices[i] - prices[i - 1], 0);//等价于上一行
-//        }
-//        return res;
-//    }
-
-    //动态规划，二维数组
-//    int maxProfit(vector<int>& prices) {
-//        vector<vector<int>> dp(prices.size(), {-prices[0], 0});
-//        for (int i = 1; i < prices.size(); ++i) {
-//            dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] - prices[i]);
-//            dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] + prices[i]);
-//        }
-//        return dp.back()[1];
-//    }
-    //动态规划，一维数组
-    int maxProfit(vector<int>& prices) {
-        vector<int> dp{-prices[0], 0};
+    //二维数组
+/*    int maxProfit(vector<int>& prices) {
+        vector<vector<int>> dp(prices.size(), {0, -prices[0], 0, -prices[0], 0});
         for (int i = 1; i < prices.size(); ++i) {
-            dp[0] = max(dp[0], dp[1] - prices[i]);
-            dp[1] = max(dp[1], dp[0] + prices[i]);
+            dp[i][0] = dp[i - 1][0];
+            dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+            dp[i][2] = max(dp[i - 1][2], dp[i - 1][1] + prices[i]);
+            dp[i][3] = max(dp[i - 1][3], dp[i - 1][2] - prices[i]);
+            dp[i][4] = max(dp[i - 1][4], dp[i - 1][3] + prices[i]);
+        }
+        return dp.back()[4];
+    }*/
+    //一维数组
+    int maxProfit(vector<int>& prices) {
+        vector<int> dp(5, 0);
+        dp[1] = dp[3] = -prices[0];
+        for (int i = 1; i < prices.size(); ++i) {
+            dp[1] = max(dp[1], - prices[i]);
+            dp[2] = max(dp[2], dp[1] + prices[i]);
+            dp[3] = max(dp[3], dp[2] - prices[i]);
+            dp[4] = max(dp[4], dp[3] + prices[i]);
         }
         return dp.back();
     }
