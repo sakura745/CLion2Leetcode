dc138457a7c0114975dd0a416ef625f73cc0e7b0
carbo <git@github.com:sakura745/C-.git>
2023-07-01 11:19:39 +0800

    Jul 1, 2023

--- leetcode/editor/cn/1035-uncrossed-lines.cpp
+++ leetcode/editor/cn/1035-uncrossed-lines.cpp
@@ -1,33 +1,58 @@
 /**
-给两个整数数组 nums1 和 nums2 ，返回 两个数组中 公共的 、长度最长的子数组的长度 。 
+在两条独立的水平线上按给定的顺序写下 nums1 和 nums2 中的整数。 
+
+ 现在，可以绘制一些连接两个数字 nums1[i] 和 nums2[j] 的直线，这些直线需要同时满足满足： 
+
+ 
+ nums1[i] == nums2[j] 
+ 且绘制的直线不与任何其他连线（非水平线）相交。 
+ 
+
+ 请注意，连线即使在端点也不能相交：每个数字只能属于一条连线。 
+
+ 以这种方法绘制线条，并返回可以绘制的最大连线数。 
 
  
 
  示例 1： 
+ 
+ 
+输入：nums1 = [1,4,2], nums2 = [1,2,4]
+输出：2
+解释：可以画出两条不交叉的线，如上图所示。 
+但无法画出第三条不相交的直线，因为从 nums1[1]=4 到 nums2[2]=4 的直线将与从 nums1[2]=2 到 nums2[1]=2 的直线相交。
 
  
-输入：nums1 = [1,2,3,2,1], nums2 = [3,2,1,4,7]
-输出：3
-解释：长度最长的公共子数组是 [3,2,1] 。
- 
 
+ 
  示例 2： 
+ 
 
  
-输入：nums1 = [0,0,0,0,0], nums2 = [0,0,0,0,0]
-输出：5
+输入：nums1 = [2,5,1,2,5], nums2 = [10,5,2,1,5,2]
+输出：3
  
+
+ 
+ 示例 3： 
+ 
+
+ 
+输入：nums1 = [1,3,7,1,7,5], nums2 = [1,9,2,5,1]
+输出：2 
 
  
 
  提示： 
 
  
- 1 <= nums1.length, nums2.length <= 1000 
- 0 <= nums1[i], nums2[i] <= 100 
+ 1 <= nums1.length, nums2.length <= 500 
+ 1 <= nums1[i], nums2[j] <= 2000 
  
 
- Related Topics 数组 二分查找 动态规划 滑动窗口 哈希函数 滚动哈希 👍 950 👎 0
+ 
+
+ Related Topics 数组 动态规划 👍 465 👎 0
 
 */
 #include<bits/stdc++.h>
@@ -53,35 +78,17 @@ using namespace std;
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
 public:
-    //二维数组
-//    int findLength(vector<int>& nums1, vector<int>& nums2) {
-//        vector<vector<int>> dp(nums1.size() + 1, vector<int>(nums2.size() + 1, 0));
-//        int res = 0;
-//        for (int i = 1; i <= nums1.size(); ++i) {
-//            for (int j = 1; j <= nums2.size(); ++j) {
-//                if (nums1[i - 1] == nums2[j - 1]) {
-//                    dp[i][j] = dp[i - 1][j - 1] + 1;
-//                }
-//                res = max(res, dp[i][j]);
-//            }
-//        }
-//        return res;
-//    }
-    //一维数组
-    int findLength(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> dp(nums2.size() + 1, 0);
-        int res = 0;
+    int maxUncrossedLines(vector<int>& nums1, vector<int>& nums2) {
+        vector<vector<int>> dp(nums1.size() + 1, vector<int>(nums2.size() + 1, 0));
         for (int i = 1; i <= nums1.size(); ++i) {
-            for (int j = nums2.size(); j > 0; --j) {
-                if (nums1[i - 1] == nums2[j - 1]) {
-                    dp[j] = dp[j - 1] + 1;
-                } else {
-                    dp[j] = 0;//恢复数组的值，以达到压缩数组的效果
-                }
-                res = max(res, dp[j]);
+            for (int j = 1; j <= nums2.size(); ++j) {
+                if (nums1[i - 1] == nums2[j - 1])
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                else
+                    dp[i][j] = max(dp[i][j - 1], dp[i - 1][j]);
             }
         }
-        return res;
+        return dp.back().back();
     }
 };
 //leetcode submit region end(Prohibit modification and deletion)
--- leetcode/editor/cn/53-maximum-subarray.cpp
+++ leetcode/editor/cn/53-maximum-subarray.cpp
@@ -66,13 +66,25 @@ using namespace std;
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
 public:
+    //贪心算法
+//    int maxSubArray(vector<int>& nums) {
+//        int res = INT_MIN;
+//        int sum = 0;
+//        for (auto& i : nums) {
+//            sum += i;
+//            if (sum > res) res = sum;
+//            if (sum <= 0) sum = 0;
+//        }
+//        return res;
+//    }
+
+    //动态规划
     int maxSubArray(vector<int>& nums) {
-        int res = INT_MIN;
-        int sum = 0;
-        for (auto& i : nums) {
-            sum += i;
-            if (sum > res) res = sum;
-            if (sum <= 0) sum = 0;
+        vector<int> dp(nums.size(), nums[0]);
+        int res = nums[0];//保存为nums.size() == 1时的结果。
+        for (int i = 1; i < nums.size(); ++i) {
+            dp[i] = max(dp[i - 1] + nums[i], nums[i]);
+            res = max(res, dp[i]);
         }
         return res;
     }
@@ -83,6 +95,8 @@ public:
 int main()
 {
     Solution s;
+    vector<int> a{-2,1,-3,4,-1,2,1,-5,4};
+    s.maxSubArray(a);
 //    vector<int> a /*initilization*/;
 //    auto x = s. /*function_name*/;
 //    cout << x << endl;
